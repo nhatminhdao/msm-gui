0a013c5d972217c7d6a1c6fafddf00f73aa47563
nhatminhdao <nhatminhdao@gmail.com>
2024-07-30 21:00:50 +0000

    final

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,48 @@
 class ActorsController < ApplicationController
+  def update
+  
+    a_id = params.fetch("path_id")
+
+    matching_records = Actor.where({ :id => a_id})
+
+    the_actor = matching_records.first
+
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_actor.image = params.fetch("the_image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+
+  end
+  
+  def create
+    d = Actor.new
+    d.name = params.fetch("the_name")
+    d.dob = params.fetch("the_dob")
+    d.bio = params.fetch("the_bio")
+    d.image = params.fetch("the_image")
+ 
+    d.save
+
+    redirect_to("/actors")
+
+  end
+
+  def destroy
+    actor_to_del_id = params.fetch("path_id")
+
+    matching_records = Actor.where({ :id => actor_to_del_id})
+
+    actor_to_del = matching_records.first
+
+    actor_to_del.destroy
+
+    redirect_to("/actors")
+  end
+  
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+
+  post("/insert_actor", { :controller => "actors", :action => "create"})
+  get("/delete_actor/:path_id", { :controller => "actors", :action => "destroy"})
+  post("/update_actor/:path_id", { :controller => "actors", :action => "update"})
 end
