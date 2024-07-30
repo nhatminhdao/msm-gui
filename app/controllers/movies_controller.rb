dd8f6d04a0c6cc3eb4eb038a5b33ed8c31dbc5c4
nhatminhdao <nhatminhdao@gmail.com>
2024-07-30 20:05:22 +0000

    done movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,25 @@
 class MoviesController < ApplicationController
+  def update
+  
+    m_id = params.fetch("path_id")
+
+    matching_records = Movie.where({ :id => m_id})
+
+    the_movie = matching_records.first
+
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.year = params.fetch("the_year")
+    the_movie.director_id = params.fetch("the_director_id")
+    the_movie.title = params.fetch("the_title")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
+
+  end
+  
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
@@ -25,7 +46,7 @@ class MoviesController < ApplicationController
 
     redirect_to("/movies")
   end
-  
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
